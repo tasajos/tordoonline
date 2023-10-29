09d6eb1c14d73b5913a21cea1d7d28f12303e2ed
tasajos <tasajos@gmail.com>
2023-10-29 15:39:30 -0400

    update site

--- tordoonline/src/app/Components/menu/menu.component.ts
+++ tordoonline/src/app/Components/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-menu',
--- tordoonline/src/app/app.module.ts
+++ tordoonline/src/app/app.module.ts
@@ -1,9 +1,16 @@
+//angular
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
+
+//trabajo
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './Components/principal/principal.component';
 import { MenuComponent } from './Components/menu/menu.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { SlideComponent } from './Components/principal/slide/slide.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,7 +29,9 @@ import { CabecerxComponent } from './Components/principal/cabecerx/cabecerx.comp
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
