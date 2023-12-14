6d5f7c9092372626f6324aba1c6de29079a96401
tasajos <tasajos@gmail.com>
2023-12-14 06:09:38 -0400

    update endpoint

--- tordoonline/src/environments/environment.ts
+++ tordoonline/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     endpoint: 'https://localhost:7196/',
+    endpointgeneraqr: 'https://dev-sip.mc4.com.bo:8443/api/v1/generaQr',
 
     //production
    // endpoint: 'http://54.213.112.54:5201/',
