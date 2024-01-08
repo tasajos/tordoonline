55d06c730d559b668d0f5b9393bd64ad6662d576
tasajos <tasajos@gmail.com>
2024-01-08 14:00:48 -0400

    udpate tabla_form

--- tordoonlineapp/src/app/Components/pasajes/pasajes.component.ts
+++ tordoonlineapp/src/app/Components/pasajes/pasajes.component.ts
@@ -79,4 +79,12 @@ export class PasajesComponent implements OnInit {
     }
   }
   
+  chunkArray(array: any[], size: number) {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+    }
+    return result;
+  }
+
 }
